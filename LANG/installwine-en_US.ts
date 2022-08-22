a38ec693097753ea1f6995b9880de07acd72a8d1
gfdgd_xi <3025613752@qq.com>
2022-08-22 17:30:42 +0800

    新增程序翻译

---
+++ LANG/installwine-en_US.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../wine-source/mainwindow.ui" line="14"/>
+        <source>下载 Wine</source>
+        <translation type="unfinished">Download Wine</translation>
+    </message>
+    <message>
+        <location filename="../wine-source/mainwindow.ui" line="41"/>
+        <source>&lt;&lt;</source>
+        <translation type="unfinished">&lt;&lt;</translation>
+    </message>
+    <message>
+        <location filename="../wine-source/mainwindow.ui" line="48"/>
+        <source>&gt;&gt;</source>
+        <translation type="unfinished">&gt;&gt;</translation>
+    </message>
+    <message>
+        <location filename="../wine-source/mainwindow.ui" line="77"/>
+        <source>不解压Wine资源文件</source>
+        <translation type="unfinished">Don&apos;t Unzip Wine File</translation>
+    </message>
+    <message>
+        <location filename="../wine-source/mainwindow.ui" line="84"/>
+        <source>删除下载的资源包，只解压保留（两个选项都选相互抵消）</source>
+        <translation type="unfinished">Delete Download Zip File And Only Unzip</translation>
+    </message>
+</context>
+</TS>
