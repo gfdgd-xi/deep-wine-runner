3a6cb7ebd2f09f5da759b4bbc3cf0fd79f533916
gfdgd_xi <3025613752@qq.com>
2023-01-06 11:28:58 +0800

    3.1.0 Complete

--- mainwindow.py
+++ mainwindow.py
@@ -2396,7 +2396,7 @@ Deepin 官网：https://www.deepin.org
 Deepin 论坛：https://bbs.deepin.org
 论坛：https://gfdgdxi.flarum.cloud/</pre>
 <hr>
-<h1>©2020~{time.strftime("%Y")} RacoonGX 团队，By gfdgd xi、为什么您不喜欢熊出没和阿布呢</h1>'''
+<h1>©2020~{time.strftime("%Y")} By gfdgd xi、为什么您不喜欢熊出没和阿布呢，RacoonGX 团队作品</h1>'''
 title = "Wine 运行器 {}".format(version)
 #<h1>©2020~{time.strftime("%Y")} <a href="https://gitee.com/gfdgd-xi">RacoonGX 团队，By gfdgd xi、为什么您不喜欢熊出没和阿布呢</h1>'''
 updateThings = "{} 更新内容：\n{}\n更新时间：{}".format(version, updateThingsString, updateTime, time.strftime("%Y"))
@@ -2562,7 +2562,7 @@ mainLayout.addWidget(returnText, 0, 1, 2, 1)
 
 # 版权
 copy = QtWidgets.QLabel(f"""\n程序版本：{version}，<b>提示：Wine 无法运行所有的 Windows 程序，如果想要运行更多可执行程序，可以考虑虚拟机和双系统</b><br>
-©2020~{time.strftime("%Y")} RacoonGX 团队，By gfdgd xi、为什么您不喜欢熊出没和阿布呢""")
+©2020~{time.strftime("%Y")} gfdgd xi、为什么您不喜欢熊出没和阿布呢，RacoonGX 团队作品""")
 mainLayout.addWidget(copy, 2, 0, 1, 1)
 
 # 程序运行
