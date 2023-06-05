6201ec2ed63cd21e8775eca89e25840afd563912
gfdgd_xi <3025613752@qq.com>
2023-06-05 20:02:01 +0800

    修复一些问题

--- wineautobuild-action-7z-aarch64.sh
+++ wineautobuild-action-7z-aarch64.sh
@@ -17,7 +17,7 @@ sudo apt install fakeroot p7zip-full -y
 sudo apt install flex bison libfreetype6-dev libjpeg-dev libpng-dev libxslt1-dev libxml2-dev libxrender-dev libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev prelink libasound2-dev -y
 sudo apt install flex bison gettext make gcc  gcc-mingw-w64 libasound2-dev libasound2-dev libpulse-dev libpulse-dev -y
 sudo apt install libdbus-1-dev libdbus-1-dev libfontconfig1-dev libfontconfig1-dev libfreetype6-dev libfreetype6-dev libgnutls28-dev libgnutls28-dev libpng-dev libpng-dev libtiff-dev libtiff-dev libgl-dev libgl-dev libunwind-dev libunwind-dev libx11-dev libx11-dev libxml2-dev libxml2-dev libxslt1-dev libxslt1-dev libgstreamer1.0-dev -y
-sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-base1.0-dev libmpg123-dev libmpg123-dev libosmesa6-dev libosmesa6-dev libudev-dev libudev-dev libvkd3d-dev libvkd3d-dev libvulkan-dev libvulkan-dev libcapi20-dev libcapi20-dev liblcms2-dev liblcms2-dev libcups2-dev libcups2-dev libgphoto2-dev libgphoto2-dev libsane-dev libsane-dev libgsm1-dev libgsm1-dev libkrb5-dev libkrb5-dev libldap2-dev libldap2-dev samba-dev ocl-icd-opencl-dev ocl-icd-opencl-dev libpcap-dev libpcap-dev libusb-1.0-0-dev libusb-1.0-0-dev libv4l-dev libv4l-dev libopenal-dev libopenal-dev libxcomposite-dev libxcomposite-dev libxcursor-dev libxcursor-dev libxi-dev libxi-dev libxrandr-dev libxrandr-dev libxinerama-dev libxinerama-dev -y
+sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-base1.0-dev libmpg123-dev libmpg123-dev libosmesa6-dev libosmesa6-dev libudev-dev libudev-dev   libvulkan-dev libvulkan-dev   liblcms2-dev liblcms2-dev libcups2-dev libcups2-dev libgphoto2-dev libgphoto2-dev libsane-dev libsane-dev   libkrb5-dev libkrb5-dev libldap2-dev libldap2-dev samba-dev   libpcap-dev libpcap-dev libusb-1.0-0-dev libusb-1.0-0-dev libv4l-dev libv4l-dev   libxcomposite-dev libxcomposite-dev libxcursor-dev libxcursor-dev libxi-dev libxi-dev libxrandr-dev libxrandr-dev libxinerama-dev libxinerama-dev -y
 # 获取数据
 url=`cat /wine-url.txt`
 version=`cat /wine-version.txt`
@@ -29,7 +29,7 @@ tar -xvf `basename $url`
 ## 编译64位
 mkdir build64
 cd build64
-sudo apt install libpcsclite-dev libsdl2-dev samba-dev -y
+sudo apt install libpcsclite-dev  samba-dev -y
 ../wine-$version/configure 
 make -j$cpu
 mkdir ../program
