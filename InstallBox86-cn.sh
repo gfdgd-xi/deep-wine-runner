0f96e1d91ab6a7408605328de17868dfc37dc66d
gfdgd_xi <3025613752@qq.com>
2023-09-10 09:07:45 +0800

    修改box86安装器至使用自建源

--- InstallBox86-cn.sh
+++ InstallBox86-cn.sh
@@ -6,43 +6,26 @@ if [[ `whoami` != root ]]; then
     exit 1
 fi
 PCArch=`dpkg --print-architecture`
-if [[ $PCArch != "aarch64" ]] && [[ $PCArch != "arm64" ]]; then
-    echo 非 ARM 架构，无法继续
-    echo 按任意键退出
-    read
-    exit 1
-fi
-which box86
-if [[ $? == 0 ]]; then
-    echo 已安装，结束
-    echo 按回车键退出
-    read 
-    exit 1
-fi
 echo 使用国内源
 #sudo wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list
-sudo bash -c "echo deb http://seafile.jyx2048.com:2345/spark-deepin-wine-runner/data/box86-debs/debian ./ > /etc/apt/sources.list.d/box86.list"
-wget -qO- http://seafile.jyx2048.com:2345/spark-deepin-wine-runner/data/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg 
-echo "adding key..."
-installBox=box86-generic-arm
-if [[ $PCArch == "arm64" ]]; then
-    sudo bash -c "echo deb http://seafile.jyx2048.com:2345/spark-deepin-wine-runner/data/box64-debs/debian ./ > /etc/apt/sources.list.d/box64.list"
-    wget -qO- http://seafile.jyx2048.com:2345/spark-deepin-wine-runner/data/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
-    installBox="box86-generic-arm box64-generic-arm"
-    sudo dpkg --add-architecture armhf
-fi
+#sudo bash -c "echo deb http://seafile.jyx2048.com:2345/spark-deepin-wine-runner/data/box86-debs/debian ./ > /etc/apt/sources.list.d/box86.list"
+#wget -qO- http://seafile.jyx2048.com:2345/spark-deepin-wine-runner/data/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg 
+#echo "adding key..."
+#installBox=box86-generic-arm
+#if [[ $PCArch == "arm64" ]]; then
+#    sudo bash -c "echo deb http://seafile.jyx2048.com:2345/spark-deepin-wine-runner/data/box64-debs/debian ./ > /etc/apt/sources.list.d/box64.list"
+#    wget -qO- http://seafile.jyx2048.com:2345/spark-deepin-wine-runner/data/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
+#    installBox="box86-generic-arm box64-generic-arm"
+#    sudo dpkg --add-architecture armhf
+#fi
+wget http://deb.box86.wine-runner.gfdgdxi.top/sources/github.sh
+bash github.sh
+rm github.sh
 echo " + sudo apt update"
 sudo apt update
 sudo apt install binfmt-support -y
 sudo apt install libc6:armhf -y
-echo apt 源添加完毕！按回车键继续安装 Box86/Box64，若想要停止安装，则请按 Ctrl+C 或按终端右上角 ×（叉叉）退出
-echo 按回车键后将会自动安装包名为 box86-generic-arm、box64-generic-arm（box64 要在 aarch64 系统才可安装）的 Box86/Box64 包（适用于通用 ARM 系统）
-echo 其他特殊版本及其包名可见如下链接（如适用于 rk3399 的版本）：
-echo "    - Box64：https://github.com/ryanfortner/box64-debs/"
-echo "    - Box86：https://github.com/ryanfortner/box86-debs/"
-echo "    - Box64（国内镜像）：http://seafile.jyx2048.com:2345/spark-deepin-wine-runner/data/box64-debs"
-echo "    - Box64（国内镜像）：http://seafile.jyx2048.com:2345/spark-deepin-wine-runner/data/box86-debs"
-read
-sudo apt install -y $installBox
+sudo apt install -y box86 
+sudo apt install -y box64
 echo 安装完成！按回车键退出
 read
