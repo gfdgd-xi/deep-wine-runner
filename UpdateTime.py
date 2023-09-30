61ea114669a34441f2a3731595dd611951058b81
gfdgd_xi <3025613752@qq.com>
2023-09-30 10:17:59 +0800

    调整自动构建

--- UpdateTime.py
+++ UpdateTime.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # 更新构建时间
 import os
-import sys
 import json
 import platform
 import datetime
 programPath = os.path.split(os.path.realpath(__file__))[0]  # 返回 string
-with open(sys.argv[1], "r") as file:
+with open(f"{programPath}/deb/opt/apps/deepin-wine-runner/information.json", "r") as file:
     info = json.loads(file.read())
 info["Time"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + platform.platform()
-with open(sys.argv[1], "w") as file:
+with open(f"{programPath}/deb/opt/apps/deepin-wine-runner/information.json", "w") as file:
     file.write(json.dumps(info, ensure_ascii=False, indent=4))
