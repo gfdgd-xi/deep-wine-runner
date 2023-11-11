1a786145fb30a6c0df9a6adf37030f9f4a33fd97
gfdgd_xi <3025613752@qq.com>
2023-11-11 10:04:00 +0800

    优化安装量检测

--- Download.py
+++ Download.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+import os
 import sys
+import json
 import base64
 import requests
-print(requests.get(base64.b64decode("aHR0cDovLzEyMC4yNS4xNTMuMTQ0L3NwYXJrLWRlZXBpbi13aW5lLXJ1bm5lci9JbnN0YWxsLnBocD9WZXJzaW9uPQ==").decode("utf-8") + sys.argv[1]).text)
+# 读取版本号
+programPath = os.path.split(os.path.realpath(__file__))[0]  # 返回 string
+with open(f"{programPath}/information.json") as file:
+    version = json.loads(file.read())["Version"]
+print(requests.get(base64.b64decode("aHR0cDovLzEyMC4yNS4xNTMuMTQ0L3NwYXJrLWRlZXBpbi13aW5lLXJ1bm5lci9JbnN0YWxsLnBocD9WZXJzaW9uPQ==").decode("utf-8") + version).text)
