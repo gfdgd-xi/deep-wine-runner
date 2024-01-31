c46aa8fbea5210599e79be267bacdea8451c9662
gfdgd_xi <3025613752@qq.com>
2024-01-31 22:06:36 +0800

    修改VMInstaller Qemu安装脚本以支持Arch Linux

--- QemuSystemInstall.sh
+++ QemuSystemInstall.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# 如果是 Arch Linux
+if [[-f /etc/arch-release ]]; then
+    sudo pacman -Syu
+    sudo pacman -S qemu-user qemu-user-static qemu-full
+    exit
+fi
 sudo apt update
 sudo apt install qemu-system qemu-user qemu-efi qemu-efi-aarch64 qemu-efi-arm -y
 sudo apt install qemu-user-static binfmt-support qemu-system-gui -y
