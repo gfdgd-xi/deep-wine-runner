db72e661a1c62084085c32cf917eba346e684ba7
gfdgd_xi <3025613752@qq.com>
2024-06-16 22:13:13 +0800

    调整run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 export PATH=/opt/apps/deepin-wine-runner-qemu-system-extra/files/usr/local/bin:$PATH
 echo 推荐使用 Wine 运行器的虚拟机管理工具调用 Qemu,如果安装了 Wine 运行器输入命令 
 echo /opt/apps/deepin-wine-runner/StartVM.sh
@@ -9,3 +8,4 @@ echo 也可以通过命令调用，输入 qemu-system-x86_64 开始吧
 echo 当前 Qemu 版本：
 qemu-system-x86_64 --version
 bash $@
+exit $?
