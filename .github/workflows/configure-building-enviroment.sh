341cbb5f1ed1c20dc08076addb7e8aab59c5751a
gfdgd_xi <3025613752@qq.com>
2024-06-23 10:24:20 +0800

    写反了

--- .github/workflows/configure-building-enviroment.sh
+++ .github/workflows/configure-building-enviroment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export DEBIAN_FRONTEND=noninteractive  # 防止卡 tzdate
 mkdir -p /etc/apt/sources.list.d/
 cp /etc/apt/sources.list /etc/apt/sources.list.d/sources.list
 sed -i "s/deb /deb-src /g" /etc/apt/sources.list.d/sources.list
@@ -16,7 +17,7 @@ fi
 apt update
 apt install sudo neofetch -y
 neofetch
-apt build-dep qemu -y | echo -e "1\n1\n1\n1\n"
+apt build-dep qemu -y
 # 如果是 Debian10 就需要安装 Python3 的依赖
 apt build-dep python3.7 -y
 apt build-dep . -y
