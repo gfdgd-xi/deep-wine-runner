829b95644a03bdcf52d3d40add72805ddcaf7434
gfdgd_xi <3025613752@qq.com>
2024-06-23 15:45:09 +0800

    新增arm

---
+++ .github/workflows/configure-building-enviroment-without-x86.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# $1=arm64
+# $2=buster
+sudo apt update
+sudo apt install debootstrap qemu-user-static -y
+bottlePath=./system-bottle
+sudo debootstrap --arch=$1 $2 $bottlePath
+sudo bash .github/workflows/pardus-chroot $bottlePath
+# 配置 git
+sudo chroot $bottlePath apt update
+sudo chroot $bottlePath apt install git -y
+sudo chroot $bottlePath git clone https://github.com/gfdgd-xi/deep-wine-runner-qemu-system
+sudo chroot $bottlePath bash -c 'cd deep-wine-runner-qemu-system; bash .github/workflows/configure-building-enviroment-without.sh'
+exit 0
---
+++ .github/workflows/run-command-in-chroot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+bottlePath=./system-bottle
+gitPath=deep-wine-runner-qemu-system
+sudo chroot $bottlePath bash -c "cd $gitPath; $@"
