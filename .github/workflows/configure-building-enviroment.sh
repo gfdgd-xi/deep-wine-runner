b88b11a03554ba42839a2645833519d8412cecdf
gfdgd_xi <3025613752@qq.com>
2024-06-23 08:12:59 +0800

    源码源没有被正确启用

--- .github/workflows/configure-building-enviroment.sh
+++ .github/workflows/configure-building-enviroment.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-sed -i "/deb-src/s/# //g" /etc/apt/sources.list
+cp /etc/apt/sources.list /etc/apt/sources.list.d/sources.list
+sed -i "/s/deb /deb-src /g" /etc/apt/sources.list.d/sources.list
+cat /etc/apt/sources.list
+cat /etc/apt/sources.list.d/sources.list
 apt update
-apt install sudo -y
+apt install sudo neofetch -y
+neofetch
 apt build-dep qemu -y
 # 如果是 Debian10 就需要安装 Python3 的依赖
 apt build-dep python3.7 -y
