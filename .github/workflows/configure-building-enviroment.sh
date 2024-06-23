cc03beab421bf24b691757ba0f88e210ece2dde3
gfdgd_xi <3025613752@qq.com>
2024-06-23 08:07:56 +0800

    多加了sudo

--- .github/workflows/configure-building-enviroment.sh
+++ .github/workflows/configure-building-enviroment.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sed -i "/deb-src/s/# //g" /etc/apt/sources.list
-sudo apt update
-sudo apt build-dep qemu -y
+apt update
+apt install sudo -y
+apt build-dep qemu -y
 # 如果是 Debian10 就需要安装 Python3 的依赖
-sudo apt build-dep python3.7 -y
+apt build-dep python3.7 -y
+apt build-dep . -y
 exit 0
