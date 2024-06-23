68f7699d967ba0f3d5c524f631b156965e116cd6
gfdgd_xi <3025613752@qq.com>
2024-06-23 09:04:47 +0800

    调整debian12和sid的deb-src

--- .github/workflows/configure-building-enviroment.sh
+++ .github/workflows/configure-building-enviroment.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+mkdir -p /etc/apt/sources.list.d/
 cp /etc/apt/sources.list /etc/apt/sources.list.d/sources.list
 sed -i "s/deb /deb-src /g" /etc/apt/sources.list.d/sources.list
 cat /etc/apt/sources.list
 cat /etc/apt/sources.list.d/sources.list
+# 判断系统版本
+cat /etc/issue | grep 12
+if [[ $? == 0 ]]; then  
+    echo "deb-src http://deb.debian.org/debian bookworm main" > /etc/apt/sources.list.d/debian-sources.list
+fi
+cat /etc/issue | grep sid
+if [[ $? == 0 ]]; then  
+    echo "deb-src http://deb.debian.org/debian sid main" > /etc/apt/sources.list.d/debian-sources.list
+fi
 apt update
 apt install sudo neofetch -y
 neofetch
