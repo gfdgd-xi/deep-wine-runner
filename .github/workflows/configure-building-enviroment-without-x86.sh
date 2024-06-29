4f5efa3efbdb9ed602d7e71106a4b92d3091c0aa
gfdgd_xi <3025613752@qq.com>
2024-06-29 17:09:27 +0800

    路径错误

--- .github/workflows/configure-building-enviroment-without-x86.sh
+++ .github/workflows/configure-building-enviroment-without-x86.sh
@@ -11,6 +11,6 @@ sudo chroot $bottlePath apt update
 sudo chroot $bottlePath apt install git -y
 sudo chroot $bottlePath git clone https://github.com/gfdgd-xi/deep-wine-runner-qemu-system --depth=1
 # 修改版本号
-bash .github/workflows/run-command-in-chroot.sh sed -i "'s/) UNRELEASED; urgency=medium/~$2) UNRELEASED; urgency=medium/g'" $bottlePath/deep-wine-runner-qemu-system/debian/changelog
+sudo sed -i "s/) UNRELEASED; urgency=medium/~$2) UNRELEASED; urgency=medium/g" $bottlePath/deep-wine-runner-qemu-system/debian/changelog
 bash .github/workflows/run-command-in-chroot.sh .github/workflows/configure-building-enviroment.sh
 exit 0
