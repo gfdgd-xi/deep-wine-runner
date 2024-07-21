55aeae22af54024e57d61a860a9765dd298314dc
gfdgd_xi <3025613752@qq.com>
2024-07-21 19:30:03 +0800

    新增开启VNC公共访问选项

--- mainwindow-termux-loader.sh
+++ mainwindow-termux-loader.sh
@@ -3,16 +3,22 @@ CURRENT_DIR=$(dirname $(readlink -f "$0"))
 if [[ ! -d $TMPDIR/tmp ]]; then
     mkdir -p $TMPDIR/tmp
 fi
+noVNCOption="--vnc localhost:5901"
+VNCServerOption="-localhost yes"
+if [[ -f $HOME/.config/deepin-wine-runner/vnc-public ]]; then
+    unset noVNCOption
+    unset VNCServerOption
+fi
 if [[ $DISPLAY == "" ]] && [[ $WAYLAND_DISPLAY == "" ]]; then
     # 自动配置 NoVNC
     export DISPLAY=:5
     vncserver -kill :5
-    vncserver $DISPLAY &
+    vncserver $DISPLAY $VNCServerOption &
     sleep 3
     xfwm4 &
     if [[ -f /data/data/com.termux/files/usr/bin/startxfce4 ]]; then
         startxfce4 &      
     fi
-    $CURRENT_DIR/novnc/utils/novnc_proxy --vnc localhost:5905 &
+    $CURRENT_DIR/novnc/utils/novnc_proxy --vnc localhost:5905 $noVNCOption &
 fi
 python3 $CURRENT_DIR/mainwindow.py
