ff60f695654351050c52eff08784b8a139120e34
gfdgd_xi <3025613752@qq.com>
2024-07-24 16:39:29 +0800

    debug

--- off-line-shell/run.sh
+++ off-line-shell/run.sh
@@ -3,17 +3,18 @@ SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 # /opt 目录识别
 option=""
 
-if [[ -d /usr/lib32 ]]; then
-    option="$option --dev-bind \"$SHELL_FOLDER/lib32\" /usr/lib32 "
+if [[ -d /usr/lib32 ]] && [[ -d $SHELL_FOLDER/lib32 ]]; then
+    option="$option --dev-bind $SHELL_FOLDER/lib32 /usr/lib32 "
 fi
 
-if [[ -d /usr/lib64 ]]; then
-    option="$option --dev-bind \"$SHELL_FOLDER/lib64\" /usr/lib64 "
+if [[ -d /usr/lib64 ]] && [[ -d $SHELL_FOLDER/lib64 ]]; then
+    option="$option --dev-bind $SHELL_FOLDER/lib64 /usr/lib64 "
 fi
 
 "$SHELL_FOLDER/bwrap" --dev-bind / / \
     --dev-bind "$SHELL_FOLDER/bin" /usr/bin \
     --dev-bind "$SHELL_FOLDER/lib" /usr/lib \
     --dev-bind /usr/lib/locale /usr/lib/locale \
+    --dev-bind "$SHELL_FOLDER/share" /usr/share \
     $option \
     $SHELL_FOLDER/runner/deepin-wine-runner $*
