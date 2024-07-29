ab31fc6d65d3bda50faa347bff460fa0dafdc2fd
gfdgd_xi <3025613752@qq.com>
2024-07-29 17:23:51 +0800

    补一点注释

--- globalenv.py
+++ globalenv.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
-def _init():#初始化
+
+# 用于实现主模块的变量可被子模块调用和读取
+# 要在主模块和需要的子模块分别 import globalenv
+# 然后需要在主模块进行初始化（_init），子模块不要重复 init
+# 接着即可调用 set_value 和 get_value 存放/读取变量
+def _init():  #初始化（在主模块初始化，不要在子模块重复 init）
     global _global_dict
     _global_dict = {}
 
-def set_value(key,value):
+def set_value(key :str, value):
     """ 定义一个全局变量 """
     _global_dict[key] = value
 
 """ 获得一个全局变量,不存在则返回默认值 """
-def get_value(key,defValue=None):
+def get_value(key, defValue=None):
     try:
         return _global_dict[key]
     except KeyError:
--- test.py
+++ test.py
@@ -3,11 +3,12 @@ import globalenv
 import PyQt5.QtWidgets as QtWidgets
 app = QtWidgets.QApplication(sys.argv)
 window = QtWidgets.QMainWindow()
+# globalenv 的 init 是必须的，这样才能正确的 import Wine 运行器的窗口
 globalenv._init()
-globalenv.set_value("app", app)
+globalenv.set_value("app", app)  # 用于将该部分的 app 给子模块的 Qt 控件调用以解决 UI 异常以及其它问题
 #import deepin_wine_packager
 #modules = __import__("deepin-wine-packager")
-modules = __import__("deepin-wine-easy-packager")
+modules = __import__("deepin-wine-easy-packager")  # 使用该方法可以引入带 - 文件名的模块
 #import mainwindow
 window.setCentralWidget(modules.window)
 window.show()
