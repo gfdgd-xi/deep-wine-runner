878ce96a506ec34928647e9c6f55aec27df7bed0
gfdgd_xi <3025613752@qq.com>
2025-02-05 12:39:46 +0800

    尝试loongarch64架构离线包

--- off-line-shell/run.sh
+++ off-line-shell/run.sh
@@ -11,9 +11,15 @@ if [[ -d /usr/lib64 ]] && [[ -d $SHELL_FOLDER/lib64 ]]; then
     option="$option --dev-bind $SHELL_FOLDER/lib64 /usr/lib64 "
 fi
 
+if [[ -d /usr/share/fonts ]]; then
+    option="$option --dev-bind /usr/share/fonts /usr/share/fonts "
+fi
+
 "$SHELL_FOLDER/bwrap" --dev-bind / / \
     --dev-bind "$SHELL_FOLDER/bin" /usr/bin \
+    --dev-bind "$SHELL_FOLDER/bin" /bin \
     --dev-bind "$SHELL_FOLDER/lib" /usr/lib \
+    --dev-bind "$SHELL_FOLDER/lib" /lib \
     --dev-bind /usr/lib/locale /usr/lib/locale \
     --dev-bind "$SHELL_FOLDER/share" /usr/share \
     $option \
