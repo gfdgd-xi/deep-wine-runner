acfb32d51f5d56eea9f7c3bc96f553accfd15a15
gfdgd_xi <3025613752@qq.com>
2025-02-06 17:53:28 +0800

    修复run-system-bwrap问题

--- off-line-shell/run-system-bwrap.sh
+++ off-line-shell/run-system-bwrap.sh
@@ -11,9 +11,22 @@ if [[ -d /usr/lib64 ]] && [[ -d $SHELL_FOLDER/lib64 ]]; then
     option="$option --dev-bind $SHELL_FOLDER/lib64 /usr/lib64 "
 fi
 
+if [[ -d /usr/share/fonts ]]; then
+    option="$option --dev-bind /usr/share/fonts /usr/share/fonts "
+fi
+
+if [[ -d $SHELL_FOLDER/gnemul ]]; then
+    if [[ ! -d /usr/gnemul ]]; then
+        pkexec mkdir -p /usr/gnemul
+    fi
+    option="$option --dev-bind $SHELL_FOLDER/gnemul /usr/gnemul "
+fi
+
 bwrap --dev-bind / / \
     --dev-bind "$SHELL_FOLDER/bin" /usr/bin \
+    --dev-bind "$SHELL_FOLDER/bin" /bin \
     --dev-bind "$SHELL_FOLDER/lib" /usr/lib \
+    --dev-bind "$SHELL_FOLDER/lib" /lib \
     --dev-bind /usr/lib/locale /usr/lib/locale \
     --dev-bind "$SHELL_FOLDER/share" /usr/share \
     $option \
